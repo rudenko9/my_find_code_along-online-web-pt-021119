20cecb455624457d1951dc18271e03a3ebfb0494
Nataliya R <n.rudenko@gmail.com>
2019-02-20 18:39:18 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,8 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length 
-  if yield(collection[i])
-    return colelction[i]
+  return colelction[i] 
   end
   i = i + 1
 end
